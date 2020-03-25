597eba14f94a7ee9a0843ac48911a5075a202efe
Spase007 <56656096+Spase007@users.noreply.github.com>
2020-03-25 22:58:32 +0100

    Add files via upload

---
+++ SwapNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Privremen;
+            string input = String.Empty;
+            Console.WriteLine("Vnesi go prviot broj.");
+            input = Console.ReadLine();
+            int PrvBr = Convert.ToInt32(input);
+            Console.WriteLine("Vnesi go vtoriot broj.");
+            input = Console.ReadLine();
+            int VtorBr = Convert.ToInt32(input);
+            Privremen = PrvBr;
+            Console.WriteLine("Privremeniot broj go zavzema brojot prv vnesen " + Privremen);
+            PrvBr = VtorBr;
+            Console.WriteLine("Prviot broj go zavzema brojot kako vtor vnesen " + VtorBr);
+            VtorBr = Privremen;
+            Console.WriteLine("Vtoriot Broj go zavzema brojot na privremeniot " + Privremen);
+        }
+    }
+}
